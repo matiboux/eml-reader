a933171b00e229d914a574a312f0eaacce9017c5
Matiboux <matiboux@gmail.com>
2024-06-05 01:33:03 +0200

    Move site config

---
+++ app/app/src/config.ts
@@ -0,0 +1,14 @@
+interface Site
+{
+	title?: string
+	description?: string
+	author?: string
+	themeColor?: string
+}
+
+export const site: Site = {
+	title: 'EML Reader',
+	description: 'Online tool to read and display the content of an EML file.',
+	author: 'Matiboux',
+	themeColor: '#ffffff',
+}
