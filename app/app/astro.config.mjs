486b73aaa6aa82ee960831d4b558bae60f04ac89
Matiboux <matiboux@gmail.com>
2024-06-08 23:32:22 +0200

    Configure vite to use node globals polyfill

--- app/app/astro.config.mjs
+++ app/app/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 import { i18n } from '/src/config'
 
@@ -12,6 +13,20 @@ export default defineConfig({
 				usePolling: true,
 			},
 		},
+		optimizeDeps: {
+			esbuildOptions: {
+				// Node.js global to browser globalThis
+				define: {
+					global: 'globalThis'
+				},
+				// Enable esbuild polyfill plugins
+				plugins: [
+					NodeGlobalsPolyfillPlugin({
+						buffer: true
+					})
+				]
+			}
+		}
 	},
 	i18n: i18n,
 	integrations: [
