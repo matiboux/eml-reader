8600c096e064b8de5db6fc181c19cc6bf551851a
Matiboux <matiboux@gmail.com>
2024-06-09 19:03:18 +0200

    Add menu for content

--- app/app/src/components/AppContent.svelte
+++ app/app/src/components/AppContent.svelte
@@ -10,6 +10,29 @@ export {
 	style,
 	locale,
 }
+
+type MenuOption = 'headers' | 'body' | 'attachments'
+
+let menuSelected: MenuOption = 'body'
+
+function handleMenuSelected(value: MenuOption)
+{
+	return (event: MouseEvent) =>
+	{
+		// Set the menu selected
+		menuSelected = value
+
+		// Scroll down to the menu
+		const wrapper = (event.currentTarget as HTMLElement).closest<HTMLElement>('.wrapper')
+		if (wrapper)
+		{
+			window.scrollTo({
+				top: wrapper.offsetTop - 24,
+				behavior: 'smooth',
+			})
+		}
+	}
+}
 </script>
 
 <div
@@ -21,69 +44,132 @@ export {
 	style={style}
 >
 	{#if $emlData}
-		<div class="paper-main paper">
-			<!-- From block -->
-			{#if $emlData.from}
-				<div>
-					<b>From:</b>
-					{$emlData.from.name ? `${$emlData.from.name} <${$emlData.from.email}>` : $emlData.from.email}
-				</div>
 
-				<hr />
+		<nav class="menu">
+			<a
+				class={menuSelected === 'headers' ? 'active' : ''}
+				on:click={handleMenuSelected('headers')}
+			>
+				Headers
+			</a>
+			<a
+				class={menuSelected === 'body' ? 'active' : ''}
+				on:click={handleMenuSelected('body')}
+			>
+				Body
+			</a>
+			{#if $emlData.attachments}
+				<a
+					class={menuSelected === 'attachments' ? 'active' : ''}
+					on:click={handleMenuSelected('attachments')}
+				>
+					Attachments
+				</a>
 			{/if}
+		</nav>
 
-			<!-- To block -->
-			{#if $emlData.to}
-				<div>
-					<b>To:</b>
-					{$emlData.to.name ? `${$emlData.to.name} <${$emlData.to.email}>` : $emlData.to.email}
-				</div>
+		<div class="paper-main">
+			<div class="paper paper-back">
+			</div>
 
-				<hr />
-			{/if}
+			<div class="paper paper-front">
+				{#if menuSelected === 'headers'}
+					<div>
+						<a
+							class={menuSelected === 'body' ? 'active' : ''}
+							on:click={handleMenuSelected('body')}
+						>
+							See message body
+							<span class="icon icon-[mdi--chevron-right]"></span>
+						</a>
+					</div>
 
-			<!-- Subject block -->
-			{#if $emlData.subject}
-				<div>
-					<b>Subject:</b>
-					{$emlData.subject}
-				</div>
+					{#if $emlData.attachments}
+						<hr />
 
-				<hr />
-			{/if}
+						<div>
+							<a
+								class={menuSelected === 'attachments' ? 'active' : ''}
+								on:click={handleMenuSelected('attachments')}
+							>
+								See {$emlData.attachments.length} message attachment{#if $emlData.attachments.length > 1}s{/if}
+								<span class="icon icon-[mdi--chevron-right]"></span>
+							</a>
+						</div>
+					{/if}
 
-			<!-- Body block -->
-			{#if $emlData.html || $emlData.text}
-				<div class="grow">
-					<iframe
-						class="w-full h-full"
-						srcdoc={$emlData.html || `<pre>${$emlData.text}</pre>`}
-					></iframe>
-				</div>
-			{/if}
+					{#if $emlData.headers}
+						<hr />
+
+						<ul>
+							{#each Object.entries($emlData.headers) as [key, value]}
+								<li>
+									<b>{key}:</b> {value}
+								</li>
+							{/each}
+						</ul>
+					{/if}
+
+				{:else if menuSelected === 'attachments' && $emlData.attachments}
+					<!-- Attachments block -->
+					<ul>
+						{#each $emlData.attachments as attachment}
+							<li>
+								<b>{attachment.name}</b> ({attachment.contentType})
+							</li>
+						{/each}
+					</ul>
+
+				{:else}
+					<!-- From block -->
+					{#if $emlData.from}
+						<div>
+							<b>From:</b>
+							{$emlData.from.name ? `${$emlData.from.name} <${$emlData.from.email}>` : $emlData.from.email}
+						</div>
+					{/if}
+
+					<!-- To block -->
+					{#if $emlData.to}
+						<div>
+							<b>To:</b>
+							{$emlData.to.name ? `${$emlData.to.name} <${$emlData.to.email}>` : $emlData.to.email}
+						</div>
+					{/if}
+
+					<!-- Subject block -->
+					{#if $emlData.subject}
+						<div>
+							<b>Subject:</b>
+							{$emlData.subject}
+						</div>
+					{/if}
+
+					<!-- Date block -->
+					{#if $emlData.date}
+						<div>
+							<b>Date:</b>
+							{$emlData.date.toLocaleString(locale)}
+						</div>
+					{/if}
+
+					<!-- Body block -->
+					{#if $emlData.html || $emlData.text}
+						<div class="grow">
+							<iframe
+								class="w-full h-full"
+								srcdoc={$emlData.html || `<pre>${$emlData.text}</pre>`}
+							></iframe>
+						</div>
+					{/if}
+				{/if}
+			</div>
 		</div>
 	{/if}
 
 	<div class="papers-secondary">
 		{#if $emlData && $emlData.attachments}
 		 	{#each $emlData.attachments as attachment}
-				<div class="paper paper-landscape">
-					<b>Attachment:</b>
-					{attachment.name}
-					<!-- <button
-						class="btn btn-primary"
-						on:click={() => {
-							const url = URL.createObjectURL(attachment.blob)
-							const a = document.createElement('a')
-							a.href = url
-							a.download = attachment.filename
-							a.click()
-							URL.revokeObjectURL(url)
-						}}
-					>
-						Download
-					</button> -->
-				</div>
 			{/each}
 		{/if}
 	</div>
@@ -97,72 +183,89 @@ export {
 		flex
 		flex-col
 		justify-center
-		gap-[calc(max(2.5rem,_5vh_/_1.414))]
-		max-w-[calc(min(100%,_75vh_/_1.414))]
+		gap-[calc(max(2.5rem,_5vh_/_1.294))]
+		max-w-[calc(min(100%,_75vh_/_1.294))]
 		;
+
+	.menu {
+		@apply
+			flex
+			justify-center
+			space-x-4
+			;
+
+		a {
+			@apply
+				bg-gray-200
+				text-gray-500
+				px-4
+				py-2
+				text-gray-600
+				font-semibold
+				rounded-md
+				cursor-pointer
+				;
+
+			&.active {
+				@apply
+					bg-gray-300
+					text-gray-800
+					;
+			}
+		}
+	}
 
 	.paper {
 		@apply
 			relative
+			bg-white
 			flex
 			flex-col
 			w-full
 			mx-auto
 			space-y-4
 			p-6
+			overflow-y-auto
+			rounded-lg
+			shadow-lg
+			border
+			border-[#00000011]
 			;
 
-		aspect-ratio: 1 / 1.414;
+		aspect-ratio: 1 / 1.294;
 
 		&.paper-landscape {
-			aspect-ratio: 1.414 / 1;
-		}
-
-		&, &::before {
-			@apply
-				bg-white
-				rounded-lg
-				shadow-lg
-				border
-				border-[#00000011]
-				;
-		}
-
-		&::before {
-			content: '';
-			@apply
-				absolute
-				top-0
-				left-0
-				w-full
-				h-full
-				;
-			display: block;
-			transform: rotate(-4deg);
-			margin-top: calc(-2% / 1.414);
-			margin-left: -2%;
-			z-index: -1;
+			aspect-ratio: 1.294 / 1;
 		}
 	}
 
 	.paper-main {
 		@apply
+			relative
 			min-h-[75vh]
 			;
-	}
 
-	.papers-secondary {
-		@apply
-			flex
-			w-[calc(max(10rem,_20vh_/_1.414))]
-			h-full
-			space-y-4
-			;
+		.paper-back {
+			@apply
+				absolute
+				top-0
+				left-0
+				;
+			transform: rotate(-4deg);
+			margin-top: calc(-2% / 1.294);
+			margin-left: -2%;
+			z-index: -1;
+		}
 
-		.paper {
-			&::before {
-				transform: rotate(-8deg);
-			}
+		.paper-front {
+			transition: margin-top 0.5s ease;
+		}
+
+		ul {
+			@apply
+				list-disc
+				list-inside
+				;
 		}
 	}
 }
