d2021c6f68965bd31fecadfe0c7087ac0f76ceb5
Matiboux <matiboux@gmail.com>
2025-01-22 01:56:14 +0100

    Update i18n config

--- app/app/src/config.ts
+++ app/app/src/config.ts
@@ -1,19 +1,27 @@
-import type { AstroUserConfig } from 'astro/config'
+import type { AstroConfig } from 'astro'
 
-export const i18n =
-{
-	defaultLocale: 'en',
+type LocaleKeys = Record<string, Record<string, string>>
+type I18nConfig = AstroConfig['i18n'] & { localeKeys?: LocaleKeys }
+
+export const i18n: I18nConfig = {
 	locales: [
 		{
-			codes: ['en', 'en-US'],
+			codes: ['en', 'en_US'],
 			path: 'en',
 		},
 		{
-			codes: ['fr', 'fr-FR'],
+			codes: ['fr', 'fr_FR'],
 			path: 'fr',
 		},
 	],
+	defaultLocale: 'en',
+	fallback: {
+		fr: 'en',
+	},
+	// localeKeys: {},
 	routing: {
 		prefixDefaultLocale: false,
+		redirectToDefaultLocale: true,
+		fallbackType: 'rewrite',
 	},
-} as const satisfies AstroUserConfig['i18n']
+}
